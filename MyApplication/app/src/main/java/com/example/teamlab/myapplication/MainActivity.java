a3b70e6b17a42fd4c8d0e716030d5e84b70c980a
menglin <865517964@qq.com>
2015-03-23 21:27:56 +0800

    去除actionbar

--- MyApplication/app/src/main/java/com/example/teamlab/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/teamlab/myapplication/MainActivity.java
@@ -1,48 +1,14 @@
 package com.example.teamlab.myapplication;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.view.Window;
+import android.support.v4.app.FragmentActivity;
 
-import com.example.teamlab.myapplication.activity.BaseSlidingActionBarActivity;
-import com.example.teamlab.myapplication.constant.Constant;
-import com.example.teamlab.myapplication.fragment.MenuFragment;
-import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
-
-public class MainActivity extends BaseSlidingActionBarActivity {
-
+public class MainActivity extends FragmentActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // set the content view
         setContentView(R.layout.activity_main);
-        initSlidingMenu();
-        setCircleProgressbar(true);
-        setActionBarTitle( Constant.TEST+"啊");
     }
-
-    /**
-     * 初始化滑动菜单配置信息
-     */
-    private void initSlidingMenu() {
-        setSlidingActionBarEnabled(true);
-        setBehindContentView(R.layout.menu_frame);
-
-        SlidingMenu menu = getSlidingMenu();
-        menu.setMode(SlidingMenu.LEFT);
-        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        menu.setShadowWidthRes(R.dimen.shadow_width);
-        menu.setShadowDrawable(R.drawable.shadow);
-        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-        menu.setFadeDegree(0.35f);
-
-        //设置菜单项
-        MenuFragment menuFragment=new MenuFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.menu_frame, menuFragment).commit();
-
-    }
-
-
 }
--- MyApplication/app/src/main/java/com/example/teamlab/myapplication/ModalActivity.java
+++ MyApplication/app/src/main/java/com/example/teamlab/myapplication/ModalActivity.java
@@ -1,16 +1,14 @@
 package com.example.teamlab.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcel;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 
-import com.example.teamlab.myapplication.activity.BaseActionBarActivity;
+import com.example.teamlab.myapplication.activity.BaseActivity;
 import com.example.teamlab.myapplication.parcel.ContentParcel;
 
-public class ModalActivity extends BaseActionBarActivity {
+public class ModalActivity extends BaseActivity {
 
     public static final String EXTRA_DATA = "ModalActivity.EXTRA_DATA";
 
@@ -20,30 +18,6 @@ public class ModalActivity extends BaseActionBarActivity {
         setContentView(R.layout.activity_modal);
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeAsUpIndicator(R.drawable.actionbar_back_btn);
-        setCircleProgressbar(true);
-        showContent();
     }
 
-    private void showContent() {
-
-        Bundle bundle = getIntent().getExtras();
-
-        if (null != bundle && bundle.containsKey(EXTRA_DATA)) {
-
-            ContentParcel contentParcel = bundle.getParcelable(EXTRA_DATA);
-            setActionBarTitle(contentParcel.getTitleRes());
-
-            FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
-            Fragment fragment=Fragment.instantiate(this,contentParcel.getClassName());
-            transaction.replace(R.id.modal_container,fragment);
-            transaction.commit();
-        }
-
-    }
-
-    @Override
-    protected boolean onHomeClicked() {
-        onBackPressed();
-        return true;
-    }
 }
