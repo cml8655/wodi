51f036ae13d3ba070a632acf02b0107d04cf29d1
menglin.chen <teamlab@192.168.1.131>
2015-03-27 13:26:16 +0800

    update

--- MyApplication/app/src/main/java/com/cml/wodi/view/GameDesktopView.java
+++ MyApplication/app/src/main/java/com/cml/wodi/view/GameDesktopView.java
@@ -1,11 +1,23 @@
 package com.cml.wodi.view;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewPropertyAnimator;
+import android.view.WindowManager;
 
 public class GameDesktopView extends ViewGroup {
+
+    private static final String TAG = "GameDesktopView";
+
+    private int radius;
+    private int padding = 10;
+    private int childSize;
 
     public GameDesktopView(Context context) {
         super(context);
@@ -19,15 +31,63 @@ public class GameDesktopView extends ViewGroup {
         super(context, attrs, defStyleAttr);
     }
 
+    private OnClickListener listener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ObjectAnimator.ofFloat(v, "x", v.getX(), radius + padding-childSize/2).setDuration(2000).start();
+            ObjectAnimator.ofFloat(v, "y", v.getY(), radius + padding-childSize/2).setDuration(2000).start();
+        }
+    };
+
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
         int count = getChildCount();
 
+        float angle = 360 / count;
+
         for (int i = 0; i < count; i++) {
+
             View v = getChildAt(i);
-            v.layout(l, t, r, b);
+
+            int width = v.getMeasuredWidth();
+            int circleR = radius - width / 2;
+            int halfWidth = width / 2;
+
+            double radian = changeAngelToRadian(angle * i);
+
+            int centerX = (int) (radius + circleR * Math.sin(radian)) + padding;
+            int centerY = (int) (radius + circleR * Math.cos(radian)) + padding;
+
+            Log.d(TAG, "center position:" + centerX + "," + centerY);
+
+            v.layout(centerX - halfWidth, centerY - halfWidth, centerX + halfWidth, centerY + halfWidth);
+            v.setOnClickListener(listener);
         }
 
+    }
+
+    private double changeAngelToRadian(float angle) {
+        return Math.toRadians(angle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int width = getDefaultSize(getSuggestedMinimumWidth(), widthMeasureSpec);
+        int height = getDefaultSize(getSuggestedMinimumHeight(), heightMeasureSpec);
+
+        int size = Math.min(width, height);
+        radius = (size - padding * 2) / 2;
+
+        int count = getChildCount();
+
+        for (int i = 0; i < count; i++) {
+            measureChild(getChildAt(i), size, size);
+        }
+
+        childSize = getChildAt(0).getMeasuredWidth();
+
+        setMeasuredDimension(size, size);
     }
 }
